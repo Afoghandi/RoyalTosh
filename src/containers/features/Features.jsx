fe3cc0dd7088ca0144c48ca9938090dcd4436a68
Afoghandi <ralphaeloshun@gmail.com>
2023-01-22 17:14:36 +0000

    Feature Component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './features.css';
-const Features = () => {
-	return <div>Features</div>;
+const Features = ({ title, text }) => {
+	return (
+		<div className='gpt3__features-container__feature'>
+			<div className='gpt3__features-container_feature-title'>
+				<div />
+				<h1>{title}</h1>
+				<div className='gpt3__features-container_feature-text'>{text}</div>
+			</div>
+			Features
+		</div>
+	);
 };
 
 export default Features;
