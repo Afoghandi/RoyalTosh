6bfcbd2ef52deb808f0add3d128bbd629d5fb04f
Afoghandi <ralphaeloshun@gmail.com>
2023-01-22 18:42:35 +0000

    Features Component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Menu = () => (
 			<a href='#possibility'>Open AI</a>{' '}
 		</p>
 		<p>
-			<a href='#feautures'>Case Studies</a>{' '}
+			<a href='#features'>Case Studies</a>{' '}
 		</p>
 		<p>
 			<a href='#blog'>Library</a>{' '}
