b93f20ecad5ed7b8a9d86dea1d61dcac4cd34825
Afoghandi <ralphaeloshun@gmail.com>
2023-01-26 23:05:37 +0000

    Blog Component

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './footer.css';
+
 const Footer = () => {
-	return <div>Footer</div>;
+	return <div className='gpt3__blog section__padding' id='blog'></div>;
 };
 
 export default Footer;
