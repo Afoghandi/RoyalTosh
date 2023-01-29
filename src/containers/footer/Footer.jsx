d599d01af49364fdcde8d7e0121935c25b201a26
Afoghandi <ralphaeloshun@gmail.com>
2023-01-29 13:32:27 +0000

    footer component and tab icon changed

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
-	return <div className='gpt3__blog section__padding' id='blog'></div>;
+	return (
+		<div className='gpt3__footer section__padding' id='footer'>
+			<div className='gpt3__footer-heading'>
+				<h1 className='gradient__text'>
+					Do you want to step into the future before others
+				</h1>
+			</div>
+
+			<div className='gpt3__footer-btn'>
+				<p>Request Early Access</p>
+			</div>
+			<div className='gpt3__footer-links'>
+				<div className='gpt3__footer-links_logo'>
+					<img src={gpt3Logo} alt='logo' />
+					<p>127 Billiding Aveneue. All Rights Reserved</p>
+				</div>
+				<div className='gpt3__footer-links_div'>
+					<h4>Links</h4>
+					<p>Overons</p>
+					<p>Social Media</p>
+					<p>Counters</p>
+					<p>Contact</p>
+				</div>
+				<div className='gpt3__footer-links_div'>
+					<h4>Company</h4>
+					<p>Terms & Conditions</p>
+					<p>Privacy Policy</p>
+
+					<p>Contact</p>
+				</div>
+				<div className='gpt3__footer-links_div'>
+					<h4>Get in touch</h4>
+					<p>127 Biklinding Avenue</p>
+					<p>0845-234-890</p>
+
+					<p>Contact</p>
+				</div>
+			</div>
+
+			<div className='gpt3__footer-copyright'>
+				<p> &#169; 2021 GPT-3. All rights reseverd.</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
